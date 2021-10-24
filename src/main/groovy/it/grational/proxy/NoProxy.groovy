64b6584e8393e66ce0e5e7a1c929f4e0d9f1c789
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2021-10-24 18:59:33 +0200

    Add support for port numbers, whitespaces, and leading dot in hostnames

--- src/main/groovy/it/grational/proxy/NoProxy.groovy
+++ src/main/groovy/it/grational/proxy/NoProxy.groovy
@@ -1,15 +1,50 @@
 package it.grational.proxy
 
 class NoProxy implements ExclusionList {
-	private final List list
+	private final List<SocketAddress> list
 
 	NoProxy() {
-		this.list = System.getenv().get('no_proxy')?.split(',') ?: []
+		this.list = parseEnv()
+	}
+
+	private List<SocketAddress> parseEnv() {
+		System.getenv().get('no_proxy')
+		?.tokenize(',')
+		?.collect { elem ->
+			def splitted = elem.trim().replaceFirst(/^\./,'').tokenize(':')
+			String host = splitted.first()
+			Integer port = ( splitted.size() == 1 ) ? Default.ANY.port : (splitted.last() as Integer)
+			return new InetSocketAddress(host, port)
+		} ?: []
 	}
 
 	@Override
 	Boolean exclude(URL url) {
-		return ( this.list.find { domain -> url.host.endsWith(domain) } != null )
+		String host = url.host
+		Integer port = (url.port != -1) ? url.port : defaultPortBy(url.protocol)
+		return (
+			this.list.find { addr ->
+				if ( addr.port != Default.ANY.port )
+					host.endsWith(addr.hostName) && ( port == addr.port )
+				else
+					host.endsWith(addr.hostName)
+			} != null
+		)
+	}
+
+	private Integer defaultPortBy(String protocol) {
+		Integer result = Default.HTTP.port
+		if ( protocol )
+			result = (protocol.toUpperCase() as Default).getPort()
+		return result
+	}
+
+	private enum Default {
+		ANY   { @Override Integer getPort() { 0 } },
+		HTTP  { @Override Integer getPort() { 80 } },
+		HTTPS { @Override Integer getPort() { 443 } },
+		FTP   { @Override Integer getPort() { 21 } }
+		abstract Integer getPort()
 	}
 
 }
--- src/test/groovy/it/grational/proxy/NoProxyUSpec.groovy
+++ src/test/groovy/it/grational/proxy/NoProxyUSpec.groovy
@@ -6,7 +6,7 @@ import it.grational.specification.Environment
 class NoProxyUSpec extends Specification {
 
 	@Shared environment = new Environment (
-		no_proxy: '10.0.0.2,pgol.net'
+		no_proxy: '10.0.0.2, 10.0.0.3:21,  .pgol.net , pgol.com:443 '
 	)
 
 	def "Should be capable of saying if a certain URL needs to pass through proxy or not"() {
@@ -17,13 +17,52 @@ class NoProxyUSpec extends Specification {
 			new NoProxy().exclude(url.toURL()) == expectedValue
 
 		where:
-			url                                            || expectedValue
-			'http://gitlab.pgol.net'                       || true
-			'https://gitlab.com'                           || false
-			'ftp://10.0.0.2'                               || true
-			'http://10.0.0.1'                              || false
-			'https://some.domain.it/query?param=1&param=2' || false
-			'http://gitlab.pgol.net/query?param=1&param=2' || true
+			url                                          || expectedValue
+			// 10.0.0.2 - all ports excluded (also the implicit 21, 80, 443)
+			'ftp://10.0.0.2'                             || true
+			'http://10.0.0.2'                            || true
+			'https://10.0.0.2'                           || true
+			'ftp://10.0.0.2:21'                          || true
+			'http://10.0.0.2:80'                         || true
+			'https://10.0.0.2:443'                       || true
+			'ftp://10.0.0.2:2121'                        || true
+			'http://10.0.0.2:8080'                       || true
+			'https://10.0.0.2:4433'                      || true
+			// 10.0.0.3:21 - proxy excluded only towards port 21
+			'ftp://10.0.0.3'                             || true
+			'http://10.0.0.3'                            || false
+			'https://10.0.0.3'                           || false
+			'ftp://10.0.0.3:21'                          || true
+			'http://10.0.0.3:80'                         || false
+			'https://10.0.0.3:443'                       || false
+			'ftp://10.0.0.3:2121'                        || false
+			'http://10.0.0.3:8080'                       || false
+			'https://10.0.0.3:4433'                      || false
+			// pgol.net - all ports excluded (also the implicit 21, 80, 443)
+			'ftp://ftp.pgol.net'                         || true
+			'http://gitlab.pgol.net'                     || true
+			'https://gitlab.pgol.net'                    || true
+			'ftp://ftp.pgol.net:21'                      || true
+			'http://gitlab.pgol.net:80'                  || true
+			'https://gitlab.pgol.net:443'                || true
+			'ftp://gitlab.pgol.net:2121'                 || true
+			'http://gitlab.pgol.net:8080'                || true
+			'https://gitlab.pgol.net:4433'               || true
+			// pgol.com:443
+			'ftp://ftp.pgol.com'                         || false
+			'http://gitlab.pgol.com'                     || false
+			'https://gitlab.pgol.com'                    || true
+			'ftp://ftp.pgol.com:21'                      || false
+			'http://gitlab.pgol.com:80'                  || false
+			'https://gitlab.pgol.com:443'                || true
+			'ftp://gitlab.pgol.com:2121'                 || false
+			'http://gitlab.pgol.com:8080'                || false
+			'https://gitlab.pgol.com:4433'               || false
+			// normal external domain
+			'https://gitlab.com'                         || false
+			// query parameters
+			'https://www.pgol.net/query?param=1&param=2' || true
+			'http://10.0.0.2:8080/query?param=1&param=2' || true
 	}
 
 	def "Should work also the the no_proxy environment variable has not been set"() {
